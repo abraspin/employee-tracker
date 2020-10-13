807190a18b655d7411523725e703bac55f56a978
abraspin <abrahamspindel@gmail.com>
2020-10-12 20:47:23 -0500

    add auto increment to id cols

--- employee-tracker-schema.sql
+++ employee-tracker-schema.sql
@@ -5,13 +5,13 @@ CREATE database employees_db;
 USE employees_db;
 
 CREATE TABLE departments (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(100) NULL,
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE employees (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
