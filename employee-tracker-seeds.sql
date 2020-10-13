2f596cc08e80a9175450d016fdddbe4a67379d23
abraspin <abrahamspindel@gmail.com>
2020-10-13 13:00:33 -0500

    split sql schema from seeds

--- employee-tracker-seeds.sql
+++ employee-tracker-seeds.sql
@@ -1,33 +1,4 @@
 
-DROP DATABASE IF EXISTS employees_db;
-CREATE database employees_db;
-
-USE employees_db;
-
-CREATE TABLE departments (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE roles (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(100) NULL,
-  salary DECIMAL NOT NULL,
-  department_id INT NOT NULL,
-  PRIMARY KEY (id)
-  FOREIGN KEY(department_id REFERENCES departments(id))
-);
-
-CREATE TABLE employees (
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT NOT NULL,
-  PRIMARY KEY (id)
-);
-
 INSERT INTO departments
 VALUES 
 (1, "Accounting"),
