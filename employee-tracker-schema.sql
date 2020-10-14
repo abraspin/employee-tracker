ab683c1f4a14aab80af7dc04985dadfd6de9f9a9
abraspin <abrahamspindel@gmail.com>
2020-10-13 23:08:46 -0500

    add correct foreign key to table schema

--- employee-tracker-schema.sql
+++ employee-tracker-schema.sql
@@ -3,27 +3,27 @@ DROP DATABASE IF EXISTS employees_db;
 CREATE database employees_db;
 
 USE employees_db;
-
 CREATE TABLE departments (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE roles (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(100) NULL,
-  salary DECIMAL NOT NULL,
-  department_id INT NOT NULL,
-  PRIMARY KEY (id)
-  FOREIGN KEY(department_id REFERENCES departments(id))
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(department_id) REFERENCES departments(id)
 );
 
 CREATE TABLE employees (
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT NOT NULL,
-  PRIMARY KEY (id)
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY(role_id) REFERENCES roles(id)
 );
