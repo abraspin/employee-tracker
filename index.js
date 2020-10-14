013f909860ca34d508676b7da98b918009b94e74
abraspin <abrahamspindel@gmail.com>
2020-10-14 15:57:24 -0500

    show all employeees is working

--- index.js
+++ index.js
@@ -145,9 +145,10 @@ function viewAllEmployees() {
   e1.id, e1.first_name, e1.last_name, r.title, d.name AS department, r.salary, CONCAT(e2.first_name, " ", e2.last_name) as manager 
      FROM 
        employees e1
-     INNER JOIN roles r ON e1.role_id = r.id 
+     INNER JOIN roles r ON e1.role_id = r.id  
      INNER JOIN departments d ON r.department_id = d.id
-     LEFT JOIN employees e2 ON e1.manager_id = e2.id;`;
+     LEFT JOIN employees e2 ON e1.manager_id = e2.id;
+     `;
 
   // log response to console
   connection.query(query, (err, res) => {
